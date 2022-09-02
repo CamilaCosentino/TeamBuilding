7c541703c7b002b1b36be5c867f07cfaf1b1024a
Camila Cosentino <89109209+CamilaCosentino@users.noreply.github.com>
2022-09-02 11:15:49 -0300

    Update preguntas.js

--- src/preguntas.js
+++ src/preguntas.js
@@ -4,7 +4,7 @@ export const preguntas = [
     {id:3,pregunta: "¿Quién es el mejor jefe de auladig?"},
     {id:4, pregunta: "¿Qué hobby tienes?"},
     {id:5,pregunta: "¿Qué les gusta hacer en su tiempo libre?"},
-    {id:6, pregunta:"¿Cual es tu animal favorito ?"},
+    {id:6, pregunta:"¿Cuál es tu mayor miedo?"},
     {id:7,pregunta: "¿Qué película recomendarías?"},
     {id:8, pregunta:"Si fueses un animal ¿Cuál serías?"},
     {id:9,pregunta: "¿Dónde te gustaría vivir?"},
