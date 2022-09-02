400552a53ca661cfdbedddf314429d9ae6429314
Camila Cosentino <89109209+CamilaCosentino@users.noreply.github.com>
2022-09-02 11:42:43 -0300

    Update preguntas.js

--- src/preguntas.js
+++ src/preguntas.js
@@ -5,7 +5,7 @@ export const preguntas = [
     {id:4, pregunta: "¿Qué hobby tienes?"},
     {id:5,pregunta: "¿Qué les gusta hacer en su tiempo libre?"},
     {id:6, pregunta:"¿Cuál es tu mayor miedo?"},
-    {id:7,pregunta: "¿Qué película recomendarías?"},
+    {id:7,pregunta: "¿Qué haces en tus tiempos libres cuando no estás trabajando?"},
     {id:8, pregunta:"Si fueses un animal ¿Cuál serías?"},
     {id:9,pregunta: "¿Dónde te gustaría vivir?"},
     {id:10, pregunta:"¿Cual es tu animal favorito ?"},
